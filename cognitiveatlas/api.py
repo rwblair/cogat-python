851ceae9db39ddc91abcedfbcd1f14eab2cfe77d
Ross Blair <rblair2@stanford.edu>
2017-09-07 15:18:52 -0700

    paramerterize domain name when builduing urls in api.py. Style changes

--- cognitiveatlas/api.py
+++ cognitiveatlas/api.py
@@ -8,24 +8,22 @@ functions for working with the cognitive atlas!
 
 
 """
-from cognitiveatlas.utils import DataJson
-import cognitiveatlas.utils
-import string
 import pandas
-import json
-import os
 
-__author__ = ["Poldracklab","Vanessa Sochat"]
+from cognitiveatlas.utils import DataJson
+
+__author__ = ["Poldracklab", "Vanessa Sochat"]
 __version__ = "$Revision: 1.0 $"
 __date__ = "$Date: 2015/01/23 $"
 __license__ = "BSD"
 
-  
-apiversion = "v-alpha"
+
+APIVERSION = "v-alpha"
+DOMAIN = "cognitveatlas.org"
 
 # Wrapper for the "search" query
-def search(query,silent=False):
-    '''search 
+def search(query, silent=False):
+    '''search
     Search Concepts, Theories, Assertions, Tasks, Task Batteries, and Disorders for a given keyword.
 
     :param query: str
@@ -33,47 +31,46 @@ def search(query,silent=False):
 
     '''
 
-    result = DataJson("http://cognitiveatlas.org/api/%s/search?q=%s" %(apiversion,query.replace(" ","%20")),silent=silent)
+    url = "http://{}/api/{}/search?q={}".format(DOMAIN, APIVERSION, query.replace(" ", "%20"))
+    result = DataJson(url, silent=silent)
     if not silent:
         print(result)
     return result
 
-
-
-def get_concept(id=None,name=None,contrast_id=None,silent=False):
+def get_concept(id_=None, name=None, contrast_id=None, silent=False):
     '''get_concept
     return one or more concepts
 
     :param id: Return the specified Concept.
     :param name name: Return the specified Concept.
     :param contrast_id: Return all Concepts related to the specified Contrast.
-    
+
     [no parameters] - Return all Concepts.
 
     :Example:
 
         http://cognitiveatlas.org/api/v-alpha/concept?id=trm_4a3fd79d096be
-
     '''
 
-    base_url = "http://cognitiveatlas.org/api/%s/concept" %(apiversion)
-    parameters = {"id":id,
-                  "name":name,
-                  "contrast_id":contrast_id}
-    url = generate_url(base_url,parameters)
-    result = DataJson(url,silent=silent)
+    base_url = "http://{}/api/{}/concept".format(DOMAIN, APIVERSION)
+    parameters = {
+        "id": id_,
+        "name": name,
+        "contrast_id": contrast_id
+    }
+    url = generate_url(base_url, parameters)
+    result = DataJson(url, silent=silent)
     if not silent:
         print(result)
     return result
 
-
-def get_task(id=None,name=None,silent=False):
+def get_task(id_=None, name=None, silent=False):
     '''get_task
     return one or more tasks
 
     :param id: Return the specified Task.
     :param name name: Return the specified Task.
-    
+
     [no parameters] - Return all Tasks with basic information only.
 
     :Example:
@@ -82,56 +79,58 @@ def get_task(id=None,name=None,silent=False):
 
     '''
 
-    base_url = "http://cognitiveatlas.org/api/%s/task" %(apiversion)
-    parameters = {"id":id,
-                  "name":name}
-    url = generate_url(base_url,parameters)
-    result = DataJson(url,silent=silent)
+    base_url = "http://{}/api/{}/task".format(DOMAIN, APIVERSION)
+    parameters = {
+        "id": id_,
+        "name": name
+    }
+    url = generate_url(base_url, parameters)
+    result = DataJson(url, silent=silent)
     if not silent:
         print(result)
     return result
 
-
-def get_disorder(id=None,name=None,silent=False):
+def get_disorder(id_=None, name=None, silent=False):
     '''get_disorder
     return one or more disorders
 
     :param id: Return the specified Disorder.
     :param name name: Return the specified Disorder.
-    
+
     [no parameters] - Return all Tasks with basic information only.
 
     '''
 
-    base_url = "http://cognitiveatlas.org/api/%s/disorder" %(apiversion)
-    parameters = {"id":id,
-                  "name":name}
-    url = generate_url(base_url,parameters)
-    result = DataJson(url,silent=silent)
+    base_url = "http://{}/api/{}/disorder".format(DOMAIN, APIVERSION)
+    parameters = {
+        "id": id_,
+        "name": name
+    }
+    url = generate_url(base_url, parameters)
+    result = DataJson(url, silent=silent)
     if not silent:
         print(result)
     return result
 
-
-
-def generate_url(base_url,parameters):
+def generate_url(base_url, parameters):
     '''
     Generate a complete url from a base and list of parameters
 
     :param base_url: the base url (string)
-    :param parameters: a dictionary with the keys being the parameter, values being the values of the parameter. Any values of None will not be added to the url.
-
+    :param parameters: a dictionary with the keys being the parameter, values
+     being the values of the parameter. Any values of None will not be added to
+     the url.
     '''
 
-    values = [x.replace(" ","%20") for x in list(parameters.values()) if x]
+    values = [x.replace(" ", "%20") for x in list(parameters.values()) if x]
     #keys = [key for key,val in parameters.iteritems() if val]
     keys = [key for (key, value) in list(parameters.items()) if value]
-    arguments = ["%s=%s" %(keys[i],values[i]) for i in range(len(values))]
+    arguments = ["%s=%s" %(keys[i], values[i]) for i in range(len(values))]
     arguments = "&".join(arguments)
-    return "%s?%s" %(base_url,arguments)
+    return "%s?%s" %(base_url, arguments)
 
 
-def filter_concepts(concept_id=None,contrast_id=None):
+def filter_concepts(concept_id=None, contrast_id=None):
     '''filter_concepts
     return concepts json
 
@@ -140,7 +139,6 @@ def filter_concepts(concept_id=None,contrast_id=None):
     [no parameters] - Return lookup with all concepts
 
     returns list of concepts
-
     '''
 
     # We can only have either or
@@ -149,30 +147,30 @@ def filter_concepts(concept_id=None,contrast_id=None):
 
     # Get concepts by contrast ids
     if contrast_id:
-        if isinstance(contrast_id,str):
+        if isinstance(contrast_id, str):
             contrast_id = [contrast_id]
         concepts = []
         for contrast in contrast_id:
             concepts = concepts + get_concept(contrast_id=contrast).json
-
     # Get concepts by concept ids
     elif concept_id:
         concepts = []
         for concept in concept_id:
-            concepts = concepts + get_concept(id=concept).json
-
+            concepts = concepts + get_concept(id_=concept).json
     # Build entire tree
     else:
         concepts = get_concept().json
 
     return concepts
-    
 
-def merge_df(left,right,join_column):
+def merge_df(left, right, join_column):
     '''Match tasks and conditions and put into a data frame based on match columns.
     '''
-    return pandas.merge(left,right, on=join_column, how='right')
+    return pandas.merge(left, right, on=join_column, how='right')
 
-def filter_result(triples_df,filters,column_id):
-    if isinstance(filters,str): filters = [filters]  
-    return triples_df[triples_df[column_id].isin(filters)] 
+def filter_result(triples_df, filters, column_id):
+    '''Apply list of filters to dataframe via the dataframe isin function
+    '''
+    if isinstance(filters, str):
+        filters = [filters]
+    return triples_df[triples_df[column_id].isin(filters)]
